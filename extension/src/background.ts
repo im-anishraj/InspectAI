23017a4071c7944cdfca80d35e3b50db3d85aec6
Anish Raj <xtylishanish@gmail.com>
2026-08-27 06:50:41 +0530

    feat: redirect to VS Code marketplace on Chrome extension install

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,3 +1,9 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: 'https://marketplace.visualstudio.com/items?itemName=im-anishraj.inspectai-ide-companion' });
+  }
+});
+
 chrome.action.onClicked.addListener((tab) => {
   if (tab.id) {
     chrome.tabs.sendMessage(tab.id, { type: 'TOGGLE_INSPECTAI' }).catch(err => {
